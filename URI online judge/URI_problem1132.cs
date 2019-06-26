98da57c3ae15ed70d2851995c9b235262752266f
MD.ANWAR HOSSAIN <48715379+anwarhossainbd@users.noreply.github.com>
2019-06-26 15:16:48 +0600

    Add files via upload

---
+++ URI online judge/URI_problem1132.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace URI_problem1132
+{
+    class URI_problem1132
+    {
+        static void Main(string[] args)
+        {
+            int X = int.Parse(Console.ReadLine());
+            int Y = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+
+            int temp;
+
+            if (X > Y)
+            {
+                temp = X;
+                X = Y;
+                Y = temp;
+            }
+
+            for (int i = X; i <= Y; i++)
+            {
+                if (i % 13 != 0)
+                {
+                    sum = sum + i;
+                }
+            }
+
+            Console.WriteLine(sum);
+
+            Console.ReadKey();
+
+        }
+    }
+}
