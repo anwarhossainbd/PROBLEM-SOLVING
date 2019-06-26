4b5f41387ca163166c994e0e787fbb760326e2b9
MD.ANWAR HOSSAIN <48715379+anwarhossainbd@users.noreply.github.com>
2019-06-26 16:14:27 +0600

    Add files via upload

---
+++ URI online judge/URI_problem1064.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace URI1064
+{
+    class URI_problem1064
+    {
+        static void Main(string[] args)
+        {
+
+            double[] input = new double[6];
+
+            for (int i = 0; i < 6; i++)
+            {
+                input[i] = double.Parse(Console.ReadLine());
+            }
+
+            int count = 0;
+            double sum = 0;
+
+            double avg;
+
+
+
+
+            foreach (double number in input)
+            {
+                if (number > 0)
+                {
+                    count = count + 1;
+
+                    sum = sum + number;
+
+                }
+            }
+
+            Console.WriteLine(count + " valores positivos");
+
+            avg = sum / count;
+
+            Console.WriteLine(avg.ToString("0.0"));
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
