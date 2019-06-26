8ad69681ebd03b9c32affac7ca1a398a3fa70b3e
MD.ANWAR HOSSAIN <48715379+anwarhossainbd@users.noreply.github.com>
2019-06-26 14:33:34 +0600

    Add files via upload

---
+++ URI online judge/URI_problem1154.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace URI_problem1154
+{
+    class URI
+    {
+        static void Main(string[] args)
+        {
+            int num;
+
+            int add = 0;
+            int count = 0;
+
+            while ((num = int.Parse(Console.ReadLine())) > -1)
+            {
+                add = add + num;
+                count = count + 1;
+
+
+
+
+            }
+
+
+            double d = (double)add / count;
+
+            Console.WriteLine(d.ToString("0.00"));
+
+            Console.ReadKey();
+        }
+    }
+}
