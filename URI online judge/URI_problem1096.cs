0d176e0204a5b69c0b972f0cbd415ce0c7cd9838
MD.ANWAR HOSSAIN <48715379+anwarhossainbd@users.noreply.github.com>
2019-06-26 15:42:23 +0600

    Add files via upload

---
+++ URI online judge/URI_problem1096.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace URI_problem1096
+{
+    class URI_problem1096
+    {
+       
+        
+            static void Main(string[] args)
+            {
+
+                int a = 7, b = 6, c = 5;
+
+
+                for (int i = 1; i <= 9; i = i + 2)
+                {
+                    Console.WriteLine("I={0} J={1}\nI={2} J={3}\nI={4} J={5}", i, a, i, b, i, c);
+
+
+
+                }
+
+
+
+                Console.ReadKey();
+
+
+
+
+            }
+        }
+}
