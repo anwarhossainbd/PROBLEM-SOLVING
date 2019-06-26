ad31e8a41e06a7a37506bf0d053967ff5925e50e
MD.ANWAR HOSSAIN <48715379+anwarhossainbd@users.noreply.github.com>
2019-06-26 15:17:31 +0600

    Add files via upload

---
+++ URI online judge/URI_problem1133.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace URI_problem1133
+{
+    class URI_problem1133
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+
+            int num2 = int.Parse(Console.ReadLine());
+
+
+            if (num1 > 0 && num2 > 0)
+            {
+
+
+                if (num1 < num2)
+                {
+
+
+                    for (int i = num1 + 1; i < num2; i++)
+                    {
+                        if (i % 5 == 2 || i % 5 == 3)
+                        {
+                            Console.WriteLine(i);
+                        }
+                    }
+                }
+
+                else
+                {
+
+
+                    for (int i = num2 + 1; i < num1; i++)
+                    {
+                        if (i % 5 == 2 || i % 5 == 3)
+                        {
+                            Console.WriteLine(i);
+                        }
+
+
+                    }
+
+
+                }
+
+
+
+
+
+            }
+
+
+
+
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
