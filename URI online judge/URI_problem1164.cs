fb2114f988f51102067d708e057ae1f4261df4b4
MD.ANWAR HOSSAIN <48715379+anwarhossainbd@users.noreply.github.com>
2019-06-26 14:34:19 +0600

    Add files via upload

---
+++ URI online judge/URI_problem1164.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uri_problem1164
+{
+    class URI
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+
+
+
+            for (int i = 0; i < a; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+
+                int sum = 0;
+
+                for (int j = 1; j < num; j++)
+                {
+                    if (num % j == 0)
+                    {
+                        sum = sum + j;
+                    }
+                }
+
+
+                if (sum == num)
+                {
+                    Console.WriteLine(num + " eh perfeito");
+
+                }
+
+                if (sum != num)
+                {
+                    Console.WriteLine(num + " nao eh perfeito");
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
