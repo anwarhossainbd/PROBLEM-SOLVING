6d759bf0dc85cf8ca5f9c19444bb7305cbcbc5f8
MD.ANWAR HOSSAIN <48715379+anwarhossainbd@users.noreply.github.com>
2019-06-26 15:20:06 +0600

    Add files via upload

---
+++ URI online judge/URI_problem1151.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace URI_problem1151
+{
+    class URI_problem1151
+    {
+        static void Main(string[] args)
+        {
+
+            int num = int.Parse(Console.ReadLine());
+
+            int a = 0;
+            int b = 1;
+
+            int aa = a;
+            int bb = b;
+            int c;
+            Console.Write(aa + " " + bb);
+
+            for (int i = 0; i < num - 2; i++)
+            {
+
+                c = a + b;
+                Console.Write(" " + c);
+                a = b;
+                b = c;
+
+
+
+
+            }
+
+            Console.WriteLine();
+
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
