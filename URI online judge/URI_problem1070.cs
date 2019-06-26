91a83c3fd3c76aec616ecd2b24a504cf0a71ed6f
MD.ANWAR HOSSAIN <48715379+anwarhossainbd@users.noreply.github.com>
2019-06-26 16:28:34 +0600

    Add files via upload

---
+++ URI online judge/URI_problem1070.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace URI_problem1070
+{
+    class URI_problem1070
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+
+            if (number % 2 == 0)
+            {
+                for (int i = number + 1; i < number + 12; i = i + 2)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            else
+            {
+                for (int i = number; i < number + 12; i = i + 2)
+                {
+                    Console.WriteLine(i);
+                }
+
+            }
+
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
