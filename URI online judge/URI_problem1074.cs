e5b9b7f4dec0015241df5c816357165ef572e28f
MD.ANWAR HOSSAIN <48715379+anwarhossainbd@users.noreply.github.com>
2019-06-26 15:38:33 +0600

    Add files via upload

---
+++ URI online judge/URI_problem1074.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace URI_problem1074
+{
+    class URI_problem1074
+    {
+        static void Main(string[] args)
+        {
+            int loopno = int.Parse(Console.ReadLine());
+
+            int number;
+
+
+            for (int i = 1; i <= loopno; i++)
+            {
+                number = int.Parse(Console.ReadLine());
+
+                if (number < 0 && number % 2 != 0 && number != 0)
+                {
+                    Console.WriteLine("ODD NEGATIVE");
+                }
+
+                if (number < 0 && number % 2 == 0 && number != 0)
+                {
+                    Console.WriteLine("EVEN NEGATIVE");
+                }
+
+
+                if (number > 0 && number % 2 != 0 && number != 0)
+                {
+                    Console.WriteLine("ODD POSITIVE");
+                }
+
+                if (number > 0 && number % 2 == 0 && number != 0)
+                {
+                    Console.WriteLine("EVEN POSITIVE");
+                }
+
+                if (number == 0)
+                {
+                    Console.WriteLine("NULL");
+                }
+
+
+                Console.ReadKey();
+
+            }
+        }
+}
