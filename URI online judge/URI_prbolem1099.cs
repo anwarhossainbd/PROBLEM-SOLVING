a9b05102f4a0b36c9908ff4b5e048c0529583399
MD.ANWAR HOSSAIN <48715379+anwarhossainbd@users.noreply.github.com>
2019-06-26 15:16:14 +0600

    Add files via upload

---
+++ URI online judge/URI_prbolem1099.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace URI_problem1099
+{
+    class URI_prbolem1099
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= number; i++)
+            {
+
+                string[] input = Console.ReadLine().Split();
+
+                int num1 = int.Parse(input[0]);
+                int num2 = int.Parse(input[1]);
+
+                int temp;
+
+                if (num1 > num2)
+                {
+                    temp = num1;
+                    num1 = num2;
+                    num2 = temp;
+
+
+                }
+
+                int sum = 0;
+
+                for (int ii = num1 + 1; ii < num2; ii++)
+                {
+                    if (ii % 2 != 0)
+                    {
+                        sum = sum + ii;
+                    }
+                    // Console.WriteLine(sum);
+                }
+                Console.WriteLine(sum);
+
+
+            }
+
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
