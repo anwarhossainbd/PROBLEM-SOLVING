d417eeb3c5cf77a22d1e2e6b8793b71611d74063
MD.ANWAR HOSSAIN <48715379+anwarhossainbd@users.noreply.github.com>
2019-07-01 12:50:49 +0600

    Add files via upload

---
+++ URI online judge/URI_problem1014.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace URI_problem1014
+{
+    class URI_problem1014
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            double y = double.Parse(Console.ReadLine());
+
+            double f = x / y;
+
+
+            Console.WriteLine(f.ToString("0.000") + " km/l");
+            Console.ReadKey();
+
+        }
+    }
+}
