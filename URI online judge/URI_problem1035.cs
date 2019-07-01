1f3072df481bb2b1b2267d0bb6f56746b7780f4b
MD.ANWAR HOSSAIN <48715379+anwarhossainbd@users.noreply.github.com>
2019-07-01 12:53:05 +0600

    Add files via upload

---
+++ URI online judge/URI_problem1035.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace URI_problem1035
+{
+    class URI_problem1035
+    {
+        static void Main(string[] args)
+        {
+
+            string[] input = Console.ReadLine().Split();
+            int a = int.Parse(input[0]);
+            int b = int.Parse(input[1]);
+            int c = int.Parse(input[2]);
+            int d = int.Parse(input[3]);
+
+            if (b > c && d > a && c + d > a + b && c >= 0 && d >= 0 && a % 2 == 0)
+
+            {
+                Console.WriteLine("Valores aceitos");
+            }
+
+
+            else
+            {
+                Console.WriteLine("Valores nao aceitos");
+            }
+
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
