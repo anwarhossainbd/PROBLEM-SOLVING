b917dd28f0f0d679d9e5cb2f1580e272ea3ce7a2
MD.ANWAR HOSSAIN <48715379+anwarhossainbd@users.noreply.github.com>
2019-07-01 12:51:42 +0600

    Add files via upload

---
+++ URI online judge/URI_problem1017.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uri_problem1017
+{
+    class URI_problem1017
+    {
+        static void Main(string[] args)
+        {
+            int time = int.Parse(Console.ReadLine());
+            int speed = int.Parse(Console.ReadLine());
+
+            double litter = (double)(time * speed) / 12;
+
+            Console.WriteLine(litter.ToString("0.000"));
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
