9af601c544c7913aba2222847234c1b4f4ebfe7f
MD.ANWAR HOSSAIN <48715379+anwarhossainbd@users.noreply.github.com>
2019-07-01 12:20:46 +0600

    Add files via upload

---
+++ URI online judge/URI_problem1037.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace URI_problem1037
+{
+    class URI_problem1037
+    {
+        static void Main(string[] args)
+        {
+            double num = double.Parse(Console.ReadLine());
+
+            if (num >= 0 && num <= 25)
+            {
+                Console.WriteLine("Intervalo [0,25]");
+            }
+
+            else if (num > 25 && num <= 50)
+            {
+                Console.WriteLine("Intervalo (25,50]");
+            }
+
+            else if (num > 50 && num <= 75)
+            {
+                Console.WriteLine("Intervalo (50,75]");
+            }
+            else if (num > 75 && num <= 100)
+            {
+                Console.WriteLine("Intervalo (75,100]");
+            }
+            else
+            {
+                Console.WriteLine("Fora de intervalo");
+            }
+
+
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
