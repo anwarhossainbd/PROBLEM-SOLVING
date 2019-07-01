7bc4829c6b7b9ea616e305bc65018f87dbc75faf
MD.ANWAR HOSSAIN <48715379+anwarhossainbd@users.noreply.github.com>
2019-07-01 12:54:25 +0600

    Add files via upload

---
+++ URI online judge/URI_problem1013.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uri1013
+{
+    class URI_problem1013
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            int a = int.Parse(input[0]);
+            int b = int.Parse(input[1]);
+            int c = int.Parse(input[2]);
+
+
+            int big1 = a > b ? a : b;
+            int big = big1 > c ? big1 : c;
+
+            Console.WriteLine(big + " eh o maior");
+            Console.ReadKey();
+
+
+        }
+    }
+}
