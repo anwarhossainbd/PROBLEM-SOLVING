72e5982946a072ce1197bde7cea2df04503ae9a2
MD.ANWAR HOSSAIN <48715379+anwarhossainbd@users.noreply.github.com>
2019-07-01 12:24:52 +0600

    Add files via upload

---
+++ URI online judge/URIproblem1044.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace URI1044
+{
+    class URIproblem1044
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            int a = int.Parse(input[0]);
+            int b = int.Parse(input[1]);
+
+            if (a % b == 0 || b % a == 0)
+            {
+                Console.WriteLine("Sao Multiplos");
+            }
+
+            else
+            {
+                Console.WriteLine("Nao sao Multiplos");
+            }
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
