616932360bed5a63fd59f0264162301e2bc97b84
MD.ANWAR HOSSAIN <48715379+anwarhossainbd@users.noreply.github.com>
2019-07-01 12:58:05 +0600

    Add files via upload

---
+++ URI online judge/URI_problem1011.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace URI1011
+{
+    class URI_problem1011
+    {
+        static void Main(string[] args)
+        {
+            static void Main(string[] args)
+            {
+                double r = double.Parse(Console.ReadLine());
+
+
+                double d = (double)4 / 3;
+
+                double res = d * 3.14159 * (r * r * r);
+
+                Console.WriteLine("VOLUME = {0}", res.ToString("0.000"));
+                Console.ReadKey();
+            }
+    }
+}
