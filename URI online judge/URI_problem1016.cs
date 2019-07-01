b8fd7eb5605b1adf56ec340e4ff4d5f77a9d6305
MD.ANWAR HOSSAIN <48715379+anwarhossainbd@users.noreply.github.com>
2019-07-01 12:54:57 +0600

    Add files via upload

---
+++ URI online judge/URI_problem1016.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace URI1016
+{
+    class URI_problem1016
+    {
+        static void Main(string[] args)
+        {
+
+            int a = int.Parse(Console.ReadLine());
+
+            int y = a * 2;
+
+            Console.WriteLine(y + " minutos");
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
