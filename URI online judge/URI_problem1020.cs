96e21c80362695390dd946895a78ee926eef1243
MD.ANWAR HOSSAIN <48715379+anwarhossainbd@users.noreply.github.com>
2019-07-01 12:52:38 +0600

    Add files via upload

---
+++ URI online judge/URI_problem1020.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace URI_problem1020
+{
+    class URI_problem1020
+    {
+        static void Main(string[] args)
+        {
+            int days = int.Parse(Console.ReadLine());
+
+            int year = days / 365;
+            int months = (days % 365) / 30;
+            int day = (days % 365) % 30;
+
+            Console.WriteLine(year + " ano(s)");
+            Console.WriteLine(months + " mes(es)");
+            Console.WriteLine(day + " dia(s)");
+
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
