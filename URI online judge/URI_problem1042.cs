f7d3a5c720dccf62d7058b600324b013c0ced040
MD.ANWAR HOSSAIN <48715379+anwarhossainbd@users.noreply.github.com>
2019-07-01 12:22:48 +0600

    Add files via upload

---
+++ URI online judge/URI_problem1042.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace URI1042
+{
+    class URI_problem1042
+    {
+        static void Main(string[] args)
+        {
+
+
+            string[] input = Console.ReadLine().Split();
+
+            int a = int.Parse(input[0]);
+            int b = int.Parse(input[1]);
+            int c = int.Parse(input[2]);
+
+
+            int i = a;
+            int j = b;
+            int k = c;
+
+            int temp;
+
+            if (a > b)
+            {
+                temp = a;
+                a = b;
+                b = temp;
+            }
+
+            if (a > c)
+            {
+                temp = a;
+                a = c;
+                c = temp;
+
+            }
+
+            if (b > c)
+            {
+                temp = b;
+                b = c;
+                c = temp;
+            }
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+
+
+            Console.WriteLine();
+
+            Console.WriteLine(i);
+            Console.WriteLine(j);
+            Console.WriteLine(k);
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
