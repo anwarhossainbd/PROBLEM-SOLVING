964d593c547b900385173207ce01958f9a67f598
MD.ANWAR HOSSAIN <48715379+anwarhossainbd@users.noreply.github.com>
2019-07-01 12:02:53 +0600

    Add files via upload

---
+++ URI online judge/URI_problem1060.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace URI_problem1060
+{
+    class URI_problem1060
+    {
+        static void Main(string[] args)
+        {
+            double[] number = new double[6];
+
+            for (int i = 0; i < 6; i++)
+            {
+                number[i] = double.Parse(Console.ReadLine());
+            }
+
+            int count = 0;
+
+            foreach (double num in number)
+            {
+                if (num > 0)
+                {
+                    count++;
+
+                }
+            }
+
+
+            Console.WriteLine(count + " valores positivos");
+
+
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
