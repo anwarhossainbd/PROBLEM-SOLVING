444af7cb78e2e190a088aab72cd45627be339d2e
MD.ANWAR HOSSAIN <48715379+anwarhossainbd@users.noreply.github.com>
2019-07-23 13:25:16 +0600

    Add files via upload

---
+++ URI online judge/URI_problem1007.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class URI_problem1007
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c, d;
+
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            c = int.Parse(Console.ReadLine());
+            d = int.Parse(Console.ReadLine());
+
+            int res = (a * b) - (c * d);
+
+            Console.WriteLine("DIFERENCA = {0}", res);
+            Console.ReadKey();
+
+
+        }
+    }
+}
