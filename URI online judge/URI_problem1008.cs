a7b67eddf622f75b64778e6bb664255178ad3149
MD.ANWAR HOSSAIN <48715379+anwarhossainbd@users.noreply.github.com>
2019-07-23 13:24:44 +0600

    Add files via upload

---
+++ URI online judge/URI_problem1008.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class URI_problem1008
+    {
+        static void Main(string[] args)
+        {
+            int number, hour;
+            double money;
+
+            number = int.Parse(Console.ReadLine());
+            hour = int.Parse(Console.ReadLine());
+            money = double.Parse(Console.ReadLine());
+
+            double salary = hour * money;
+
+            Console.WriteLine("NUMBER = {0}", number);
+            Console.WriteLine("SALARY = U$ {0}", salary.ToString("0.00"));
+
+            Console.ReadKey();
+
+
+
+
+
+        }
+    }
+}
