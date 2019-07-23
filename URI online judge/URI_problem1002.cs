bfe4190e7ac4ace28e09102be02cbf02a0890928
MD.ANWAR HOSSAIN <48715379+anwarhossainbd@users.noreply.github.com>
2019-07-23 13:35:48 +0600

    Add files via upload

---
+++ URI online judge/URI_problem1002.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace URI1002
+{
+    class URI_problem1002
+    {
+        static void Main(string[] args)
+        {
+            double r, a;
+            r = double.Parse(Console.ReadLine());
+            a = 3.14159 * (r * r);
+
+            Console.WriteLine("A={0}", a.ToString("0.0000"));
+            Console.ReadKey();
+
+
+        }
+    }
+}
