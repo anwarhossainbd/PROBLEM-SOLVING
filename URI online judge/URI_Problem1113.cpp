d6ee79419865929db5c839f25e3cc071af9ca30b
MD.ANWAR HOSSAIN <48715379+anwarhossainbd@users.noreply.github.com>
2020-05-03 18:01:02 +0600

    Add files via upload

---
+++ URI online judge/URI_Problem1113.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int x,y ;
+
+    while(true){
+
+        cin>>x ;
+        cin>>y ;
+
+        if(x==y){
+             break ;
+        }
+
+
+
+        if(x>y){
+             cout << "Decrescente" << endl;
+
+        }
+
+
+
+
+        else{
+             cout << "Crescente"<<endl ;
+
+        }
+
+
+    }
+}
