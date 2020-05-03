58af35f035b9c867ab597b158c34a078bb2ceb56
MD.ANWAR HOSSAIN <48715379+anwarhossainbd@users.noreply.github.com>
2020-05-03 18:02:43 +0600

    Add files via upload

---
+++ URI online judge/URI_Problem1114.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<bits/stdc++.h>
+
+using namespace std ;
+
+int main(){
+
+int value=2002  ;
+  int number=1 ;
+
+
+while(value!=number){
+
+
+    cin>>number ;
+
+    if (number!= value){
+        cout<<"Senha Invalida"<<endl ;
+    }
+    else {
+      cout<<"Acesso Permitido"<<endl   ;
+    }
+}
+}
