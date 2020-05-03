5838da5c02a84e61a56b2c7b7b70e999b7df9168
MD.ANWAR HOSSAIN <48715379+anwarhossainbd@users.noreply.github.com>
2020-05-03 18:09:07 +0600

    Add files via upload

---
+++ URI online judge/URI_Problem1155.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<bits/stdc++.h>
+
+using namespace std ;
+
+int main(){
+
+
+ double s,sum=0 ;
+
+ for(double i=1 ;i<=100 ;i=i+1){
+
+    sum =(double)sum+(1/i) ;
+
+ }
+
+ s=sum ;
+
+ cout<<fixed ;
+
+ cout<<setprecision(2) <<s<<endl ;
+
+}
