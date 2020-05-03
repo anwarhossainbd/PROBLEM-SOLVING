ab26a38649c8fbb773e121865f6ba5f964d1053f
MD.ANWAR HOSSAIN <48715379+anwarhossainbd@users.noreply.github.com>
2020-05-03 18:06:07 +0600

    Add files via upload

---
+++ URI online judge/URI_Problem1134.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+  int x=0,y=0,z=0 ;
+ int a ;
+
+
+    while(true){
+
+
+        cin>>a ;
+
+         if(a==4){
+            break ;
+         }
+
+         if(a==1){
+            x=x+1 ;
+         }
+         else if(a==2){
+
+             y=y+1 ;
+         }
+
+          else if(a==3){
+
+             z=z+1 ;
+         }
+
+
+
+    }
+
+
+ cout<<"MUITO OBRIGADO"<<endl ;
+    cout<<"Alcool: "<<x<<endl ;
+    cout<<"Gasolina: "<<y<<endl ;
+    cout<<"Diesel: "<<z<<endl ;
+
+
+
+
+
+}
+
